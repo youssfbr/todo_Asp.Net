2c917ca8495aee15fe9e78082509bd37ac9089b7
Alisson Youssf <youssfbr@gmail.com>
2021-09-28 16:22:52 -0300

    Todo

---
+++ Entities/EntityBase.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities
+{
+    public class EntityBase<T> where T : struct
+    {
+        [Key]
+        [Column("Id")]
+        public T Id { get; set; }
+    }
+}
---
+++ Entities/ToDo.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities
+{
+    [Table("Todo")]
+    public class ToDo : EntityBase<int>
+    {
+        [Required]
+        [Column("Tarefa")]
+        public string Tarefa { get; set; }
+    }
+}
